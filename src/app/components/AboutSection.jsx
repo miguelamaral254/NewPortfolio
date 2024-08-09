daeed2186d00f776de29a08a1095ca08ac654856
Miguel <miguel.amaral.sales@gmail.com>
2024-08-09 13:06:06 -0300

    text bug fix

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -4,7 +4,7 @@ import TabButton from "./TabButton";
 
 const TAB_DATA = [
   {
-    title: "HardSkills",
+    title: "Hard Skills",
     id: "hardSkills",
     content: (
       <div className="flex flex-wrap gap-5 ">
@@ -12,8 +12,8 @@ const TAB_DATA = [
           <span>Frameworks</span>
           <hr width="100%" />
           <li>Angular</li>
-          <li>Next JS</li>
-          <li>Springboot</li>
+          <li>Next.js</li>
+          <li>Spring Boot</li>
         </ul>
         <ul className="list-disc pl-2">
           <span>Operational Systems</span>
@@ -22,48 +22,45 @@ const TAB_DATA = [
           <li>MacOS X</li>
           <li>Linux</li>
         </ul>
-
         <ul className="list-disc pl-2">
-          <span>Programming languages</span>
+          <span>Programming Languages</span>
           <hr width="100%" />
-          <li>Typescript</li>
+          <li>TypeScript</li>
           <li>Java</li>
           <li>C#</li>
           <li>Python</li>
           <li>PHP</li>
         </ul>
-
         <ul className="list-disc pl-2">
-          <span>Data Base</span>
+          <span>Databases</span>
           <hr width="100%" />
-          <li>Mysql</li>
+          <li>MySQL</li>
           <li>PostgreSQL</li>
-          <li>Ms SQL server</li>
+          <li>MS SQL Server</li>
         </ul>
         <ul className="list-disc pl-2">
-          <span>Agile methodologies</span>
+          <span>Agile Methodologies</span>
           <hr width="100%" />
           <li className="pt-2">Scrum</li>
-          <li>Kanbam</li>
+          <li>Kanban</li>
         </ul>
       </div>
     ),
   },
   {
-    title: "SoftSkills",
+    title: "Soft Skills",
     id: "softSkills",
     content: (
       <div className="flex flex-wrap gap-5 ">
         <ul className="list-disc pl-2">
           <hr width="100%" />
           <li className="pt-2">Clear and effective communication</li>
-          <li>problem solving</li>
-          <li>humanized customer service</li>
-          <li>self-management</li>
-          <li>ease of working in a team and dividing tasks</li>
-          <li> emotional intelligence, </li>
-          <li>self-management</li>
-          <li>focus on delivering results</li>
+          <li>Problem-solving</li>
+          <li>Humanized customer service</li>
+          <li>Self-management</li>
+          <li>Ease of working in a team and dividing tasks</li>
+          <li>Emotional intelligence</li>
+          <li>Focus on delivering results</li>
           <hr width="100%" />
         </ul>
       </div>
@@ -75,7 +72,7 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>
-          Analysis and systems development <br />
+          Analysis and Systems Development <br />
           Senac College, Recife-PE
         </li>
         <hr width="60%" />
@@ -89,21 +86,18 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>
           Frontend Developer <br />
-          Aplantica pizzeria
+          Aplantica Pizzeria
         </li>
-
         <hr width="40%" />
         <li>
-          Fullstack developer <br />
-          Mascate Scheadule system
+          Fullstack Developer <br />
+          Mascate Schedule System
         </li>
-
         <hr width="40%" />
         <li className="pt-1">
           Frontend Developer <br />
           SERPRO
         </li>
-
         <hr width="40%" />
         <li className="pt-1">
           IT Support Analyst <br />
@@ -131,16 +125,14 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            My name is Miguel Augusto Sales do Amaral, Fullstack developer with
-            a strong focus on building APIs using mainly technologies such as
-            Java and Spring Boot. I'm also proficient in the most modern
-            technologies such as Next.js and TypeScript. I have experience in
-            creating complete and scalable solutions, using architectures such
-            as clean architecture and applying good development practices to
-            ensure efficiency and quality. I am always looking for new
-            challenges to expand my skills and contribute to innovative
+            My name is Miguel Amaral, a Fullstack developer with
+            a strong focus on building APIs using technologies such as Java and
+            Spring Boot. I&apos;m also proficient in modern technologies like
+            Next.js and TypeScript. I have experience in creating complete and scalable
+            solutions, using architectures such as clean architecture and applying
+            good development practices to ensure efficiency and quality. I am always
+            looking for new challenges to expand my skills and contribute to innovative
             projects.
-            .
           </p>
         </div>
         <div>
@@ -149,33 +141,29 @@ const AboutSection = () => {
               selectTab={() => handleTabChange("hardSkills")}
               active={tab === "hardSkills"}
             >
-              {" "}
-              Hard Skills{" "}
+              Hard Skills
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("softSkills")}
               active={tab === "softSkills"}
             >
-              {" "}
-              Soft skills{" "}
+              Soft Skills
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("education")}
               active={tab === "education"}
             >
-              {" "}
-              Education{" "}
+              Education
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("experience")}
               active={tab === "experience"}
             >
-              {" "}
-              Experience{" "}
+              Experience
             </TabButton>
           </div>
           <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
+            {TAB_DATA.find((t) => t.id === tab)?.content}
           </div>
         </div>
       </div>
